5332ded8fab6da935a03a9d384ec1c712f4b0506
chace <1154106524@qq.com>
2021-03-04 21:39:22 +0800

    add animation

--- src/views/appMarket/index.vue
+++ src/views/appMarket/index.vue
@@ -4,7 +4,6 @@
       <el-tag style="box-shadow: 0 8px 16px 0 rgba(36, 46, 66, 0.28);" effect="dark"><i class="header-icon el-icon-info" /> 功能描述：{{ tablePage.desc }}</el-tag>
     </el-row>
 
-
     <el-row style="margin-bottom: 5vh">
       <el-button icon="el-icon-plus" type="primary" circle @click="createJson(token, kind, createAbout)" />
       <el-button
@@ -17,73 +16,90 @@
     <el-divider />
 
     <el-row>
-<!--      <el-row style="margin-bottom: 2vh">-->
-        <dynamic-form
-          v-if="tablePage.dynamicFormVisible"
-          :form-data="tablePage.dynamicFormJson"
-          :kind="kind"
-          @watchSearch="search($event)"
-        />
-<!--      </el-row>-->
+      <dynamic-form
+        v-if="tablePage.dynamicFormVisible"
+        :form-data="tablePage.dynamicFormJson"
+        :kind="kind"
+        @watchSearch="search($event)"
+      />
+    </el-row>
+
+    <el-row>
       <el-radio-group v-model="chosenRadioName" @change="handleRadioClick">
-        <el-radio-button label="上海"></el-radio-button>
+        <el-radio-button label="上海" />
         <el-radio-button label="北京" />
         <el-radio-button label="广州" />
         <el-radio-button label="深圳" />
       </el-radio-group>
-
     </el-row>
-    <el-row
-      v-loading="tablePage.listLoading"
-      :gutter="20"
-    >
-      <el-col
-        v-for="(item, index) in tablePage.tableData"
-        :key="index"
-        :span="6"
-      >
-        <el-tooltip placement="top">
-          <div slot="content" style="width: 200px;">
-            <p>{{ item.json.metadata.name }}</p>
-          </div>
-          <el-card>
-            <el-row>
-              <el-col :span="8" style="margin-top: 20%">
-                <el-image
-                  style="border-radius: 2px;"
-                  :src="require('../../assets' + (item.avatar ? item.avatar : '/avatar.jpg'))"
-                  :fit="'fit'"
-                />
-              </el-col>
 
-              <el-col :span="16">
-                <el-row>
-                  <el-form label-position="right" label-width="6vw">
-                    <el-form-item v-for="(labelItem, key) in tablePage.tableColumns" :label="labelItem.label" :key="key">
-                      <el-select
-                        size="mini"
-                        v-if="labelItem.kind === 'action'"
-                        v-model="item.val"
-                        placeholder="请选择"
-                        @change="handleActionChange($event, item.json, token, kind, listQuery, tablePage, updateAbout)"
-                      >
-                        <el-option
-                          v-for="i in tablePage.actions"
-                          :key="i.key"
-                          :label="i.key"
-                          :value="i.type"
-                        />
-                      </el-select>
-                      <el-tag size="small" v-else>{{getInputValue(item.json, labelItem.row)}}</el-tag>
-                    </el-form-item>
-                  </el-form>
+    <el-row :gutter="10">
+      <el-col :span="leftSpan" style="transition: all 0.28s;">
+        <el-row
+          v-loading="tablePage.listLoading"
+          :gutter="leftGutter"
+        >
+          <el-col
+            v-for="(item, index) in tablePage.tableData"
+            :key="index"
+            style="cursor: pointer"
+            :span="4"
+            @click.native="showDetail(item)"
+          >
+            <el-tooltip placement="top">
+              <div slot="content">
+                <p>{{ item.json.metadata.name }}</p>
+              </div>
+              <el-card class="exhibition">
+                <el-row style="margin-bottom: 20px;" type="flex" justify="center">
+                  <el-col>
+                  <el-image
+                    style="border-radius: 2px;"
+                    :src="require('../../assets' + (item.avatar ? item.avatar : '/avatar.jpg'))"
+                    :fit="'fit'"
+                  />
+                  </el-col>
                 </el-row>
 
-              </el-col>
+                <el-row type="flex" justify="center" style="text-align:center">
+
+                  <el-tag>{{ item.json.metadata.name }}</el-tag>
+                </el-row>
+              </el-card>
+            </el-tooltip>
+          </el-col>
+        </el-row>
+
+      </el-col>
+
+      <transition name="el-zoom-in-top">
+        <el-col v-show="detailVisible" :span="24 - leftSpan">
+          <el-card shadow="never" style="margin-top: 30px;border: #2b2f3a 1px solid">
+            <el-row>
+              <el-form>
+                <el-form-item v-for="(labelItem, key) in tablePage.tableColumns" :key="key" :label="labelItem.label">
+                  <el-select
+                    v-if="labelItem.kind === 'action'"
+                    v-model="detailItem.val"
+                    size="mini"
+                    placeholder="请选择"
+                    @change="handleActionChange($event, detailItem.json, token, kind, listQuery, tablePage, updateAbout)"
+                  >
+                    <el-option
+                      v-for="i in tablePage.actions"
+                      :key="i.key"
+                      :label="i.key"
+                      :value="i.type"
+                    />
+                  </el-select>
+                  <el-tag v-else size="small">{{ getInputValue(detailItem.json, labelItem.row) }}</el-tag>
+                </el-form-item>
+              </el-form>
             </el-row>
           </el-card>
-        </el-tooltip>
-      </el-col>
+        </el-col>
+        </transition>
+
     </el-row>
     <el-row style="margin-top: 30px">
       <pagination
@@ -95,7 +111,6 @@
         @pagination="refresh"
       />
     </el-row>
-
     <JsonDialog
       :json-editor="createAbout.ifJsonEditorForCreate"
       :title="createAbout.createResourceTitle"
@@ -168,7 +183,12 @@ export default {
         actionDialogVisible: false,
         updateFormConfig: [],
         propertiesInfo: []
-      }
+      },
+      leftSpan: 24,
+      detailVisible: false,
+      // leftCardSpan: 3,
+      leftGutter: 20,
+      detailItem: {}
     }
   },
   created() {
@@ -200,7 +220,17 @@ export default {
     applyOperation,
     createJson,
     getInputValue,
-    handleActionChange
+    handleActionChange,
+    showDetail(detailItem) {
+      this.leftSpan = 18
+      // this.leftCardSpan = 3
+      this.leftGutter = 10
+      this.detailItem = detailItem
+      setTimeout(() => {
+        this.detailVisible = true
+      }, 280)
+      // this.detailVisible = true
+    }
   }
 }
 </script>
@@ -211,21 +241,22 @@ export default {
   font-size: 14px;
   //line-height: 1.67;
 .el-row{
-  .el-card {
+  .exhibition {
     box-shadow: 0px 1px 2px -2px rgba(0, 0, 0, 0.16),
     0px 3px 6px 0px rgba(0, 0, 0, 0.12),
     0px 5px 12px 4px rgba(0, 0, 0, 0.09);
     margin-top: 30px;
+
   }
 
-  .el-card:hover {
+  .exhibition:focus {
     box-shadow: 0px 6px 16px -8px rgba(0, 0, 0, 0.08),
     0px 9px 28px 0px rgba(0, 0, 0, 0.05),
     0px 12px 48px 16px rgba(0, 0, 0, 0.03);
     transform: translateY(-5px);
     color: yellowgreen;
     //border-top: #409EFF 1px solid;
-    border: #1f1f1f 1px solid;
+    border: #409EFF 1px solid;
   }
 }
   .el-form-item{
