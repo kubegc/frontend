a8bee65409e9990b90d99f20f7e2f86c15e016f1
chace <1154106524@qq.com>
2021-03-09 15:16:03 +0800

    modify

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.g = {
-    baseURL: (document.location.protocol === 'https:' ? 'https:' : 'http:') + "//39.106.40.190:30308/kubesys",
+    baseURL: (document.location.protocol === 'https:' ? 'https:' : 'http:') + "//39.106.40.190/kubesys",
     VUE_APP_MONITOR_HOST_GRAFANA: "39.106.40.190:31002"
 }
