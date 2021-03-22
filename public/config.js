7efe3d95ebcd23b9921f220caffbde648cd8e3bc
chace <1154106524@qq.com>
2021-03-22 14:27:13 +0800

    modify

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.g = {
-    baseURL: (document.location.protocol === 'https:' ? 'https:' : 'http:') + "//39.106.40.190:30308/kubesys",
-    VUE_APP_MONITOR_HOST_GRAFANA: "39.106.40.190:31002"
+    baseURL: "http://" + process.env.mapperIP + ":30308/kubesys",
+    VUE_APP_MONITOR_HOST_GRAFANA: process.env.grafanaIP + ":31002"
 }
