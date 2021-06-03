0fbc31f169fd311dba75a17f8ece947dba81252e
chace <1154106524@qq.com>
2021-06-03 11:13:59 +0800

    add dashboard

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <iframe id="iframe" class="iframe" :src="sourceLink" />
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      sourceLink: ''
+    }
+  },
+  created() {
+    this.sourceLink = this.$route.meta.source
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.iframe{
+  width: 100%;
+  height: 1000px;
+  border: 0ch;
+}
+</style>
