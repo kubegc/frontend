891efbd17f8c78232a3ba8c007338e6953a76fe1
syswu <wuheng@iscas.ac.cn>
2021-09-04 10:31:02 +0800

    add build.sh

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM node:14.15.0 as build
 MAINTAINER ISCAS {lichengzhi99,tangting17,wuheng}@otcaix.iscas.ac.cn
-COPY . /k8s-frontend
-RUN cd /k8s-frontend \
+COPY . /kube-frontend
+RUN cd /kube-frontend \
     && npm install \
     && npm run build:prod
 FROM nginx as nginx
-COPY --from=build /k8s-frontend/dist /usr/share/nginx/html
+COPY --from=build /kube-frontend/dist /usr/share/nginx/html
 CMD ["nginx", "-g", "daemon off;"]
---
+++ build.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+###############################################
+##
+##  Copyright (2020, ) Institute of Software
+##      Chinese Academy of Sciences
+##          wuheng@iscas.ac.cn
+##
+###############################################
+
+
+arch=""
+
+if [[ $(arch) == "x86_64" ]]
+then
+  arch="amd64"
+elif [[ $(arch) == "aarch64" ]]
+then
+  arch="arm64"
+else
+  echo "only support x86_64(amd64) and aarch64(arm64)"
+  exit 1
+fi
+
+frontend="kube-frontend"
+version=$(cat VERSION)
+
+repo="registry.cn-beijing.aliyuncs.com/doslab"
+
+docker build . -t $repo/$frontend:v$version-$arch
+
+###############################################
+##
+##  local image to remote
+##
+###############################################
+
+docker push  $repo/$frontend:v$version-$frontend
