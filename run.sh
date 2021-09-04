6098f982f55e74487382485b0f150d3fbe10854f
syswu <wuheng@iscas.ac.cn>
2021-09-04 11:37:55 +0800

    fix script bug

--- run.sh
+++ run.sh
@@ -16,8 +16,8 @@ config_name="config.js"
 rm -rf $config_dir
 mkdir -p $config_dir
 echo "window.g = {" > $config_dir/$config_name
-echo "    baseURL: 'http://$fip:30308/kubesys'" > $config_dir/$config_name
-echo "window.g = }" > $config_dir/$config_name
+echo "    baseURL: 'http://$fip:30308/kubesys'" >> $config_dir/$config_name
+echo "}" >> $config_dir/$config_name
 
 frontend="kube-frontend"
 dir="/opt/yamls"
@@ -38,13 +38,13 @@ else
 fi
 
 ## version
-version=$(cat VERSION')
+version=$(cat VERSION)
 
 ## dir
 mkdir -p $dir
 \cp yamls/$frontend.yaml $dir/$frontend.yaml
 
-sed -i "s/#token#/$token/g" $dir/$frontend.yaml
+sed -i "s/#arch#/$arch/g" $dir/$frontend.yaml
 sed -i "s/#version#/$version/g" $dir/$frontend.yaml
 
 kubectl apply -f $dir/$frontend.yaml
