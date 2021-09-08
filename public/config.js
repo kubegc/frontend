65c3a0d1138e1575bcf14843f1d55fc7f56eabd3
Heng WU <wuheng@iscas.ac.cn>
2021-09-08 15:31:19 +0800

    fix delete table bug

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.g = {
     // baseURL: 'http://39.100.91.95:30308/kubesys'
-    baseURL: 'http://192.168.90.78:30308/kubesys'
-    // baseURL: 'http://127.0.0.1:30308/kubesys'
+    // baseURL: 'http://192.168.90.78:30308/kubesys'
+    baseURL: 'http://127.0.0.1:30308/kubesys'
 }
