6e63ed1aee0e965d1890b43e00561e425a749237
Heng WU <wuheng@iscas.ac.cn>
2021-10-29 22:58:11 +0800

    fix title name bug

--- src/permission.js
+++ src/permission.js
@@ -5,7 +5,7 @@ import { Message } from 'element-ui'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import getTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 const whiteList = ['/login'] // no redirect whitelist
@@ -15,7 +15,7 @@ router.beforeEach(async(to, from, next) => {
   NProgress.start()
 
   // set page title
-  document.title = getPageTitle(to.meta.title)
+  document.title = getTitle(document)
 
   // determine whether the user has logged in
   const hasToken = getToken()
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,10 +1,21 @@
-import defaultSettings from '@/settings'
+// import defaultSettings from '@/settings'
+import { getResource } from '@/api/kubernetes'
 
-const title = defaultSettings.title || 'admin'
-
-export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
+// const title = defaultSettings.title || 'admin'
+export default function getTitle(doc) {
+  getResource({
+    token: 'default',
+    kind: 'Frontend',
+    namespace: 'default',
+    name: 'frontend-project'
+  }).then((response) => {
+    doc.title = response.data.spec.data[0].label
+  })
 }
+
+// export default function getPageTitle(pageTitle) {
+//   if (pageTitle) {
+//     return `${pageTitle} - ${title}`
+//   }
+//   return `${title}`
+// }
