1cea72f29c1a0bf49c8843759b186f6aa08b1103
yanbingshuai <1340660560@qq.com>
2022-04-11 17:44:14 +0800

    aaa

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.g = {
-    // baseURL: 'http://39.100.71.73:30308/kubesys'
-    baseURL: 'http://101.200.242.148:30308/kubesys'
+    baseURL: 'http://39.100.71.73:30308/kubesys'
+    //baseURL: 'http://101.200.242.148:30308/kubesys'
     // baseURL: 'http://192.168.188.152:30308/kubesys'
     // baseURL: 'http://127.0.0.1:30308/kubesys'
 }
