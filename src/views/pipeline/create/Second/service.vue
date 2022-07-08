cafad29fa0e66c408d22d80e65af476d58d410e7
yanbingshuai <1340660560@qq.com>
2022-07-08 18:09:32 +0800

    aaa

--- src/views/pipeline/create/Second/service.vue
+++ src/views/pipeline/create/Second/service.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="projects-guide-service-container">
+  <div class="projects-service-mgr">
     <div class="guide-container">
       <step :activeStep="1">
       </step>
@@ -18,24 +18,55 @@
       </div>
     </div>
 
-    <ServiceK8s />
-
-<!--    <div class="projects-guide-service-container">-->
-<!--      <el-drawer title="代码源集成"-->
-<!--                 :visible.sync="integrationCodeDrawer"-->
-<!--                 direction="rtl">-->
-<!--        <add-code @cancel="integrationCodeDrawer = false"></add-code>-->
-<!--      </el-drawer>-->
-<!--      <div class="guide-container">-->
-<!--        <div class="current-step-container">-->
-<!--          <div class="title-container">-->
-<!--            <span class="first">第二步</span>-->
-<!--            <span class="second">创建服务模板，后续均可在项目中重新配置</span>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <ServiceK8s />-->
-<!--    </div>-->
+    <div class="pipeline onboarding">
+      <div class="pipeline-workflow__wrap">
+        <multipane class="vertical-panes"
+                   layout="vertical">
+          <div class="service-tree-container">
+            <serviceTree></serviceTree>
+          </div>
+          <template v-if="service.service_name &&  services.length >0">
+            <template v-if="service.type==='k8s'">
+              <multipane-resizer></multipane-resizer>
+              <div class="service-editor-container"
+                   :style="{ minWidth: '300px', width: '500px'}"
+                   :class="{'pm':service.type==='pm'}">
+                <serviceEditorK8s ref="serviceEditor"
+                                  :serviceInTree="service"
+                                  :showNext.sync="showNext"
+                                  @onParseKind="getYamlKind"
+                                  @onRefreshService="getServices"
+                                  @onRefreshSharedService="getSharedServices"
+                                  @onUpdateService="onUpdateService"></serviceEditorK8s>
+              </div>
+              <multipane-resizer></multipane-resizer>
+              <aside class="pipelines__aside pipelines__aside_right"
+                     :style="{ flexGrow: 1 }">
+                <serviceAsideK8s :service="service"
+                                 :detectedEnvs="detectedEnvs"
+                                 :detectedServices="detectedServices"
+                                 :systemEnvs="systemEnvs"
+                                 @getServiceModules="getServiceModules"> </serviceAsideK8s>
+              </aside>
+            </template>
+          </template>
+          <div
+            class="no-content">
+            <img src="@/assets/icons/editor_nodata.svg"
+                 alt="">
+            <p v-if="services.length === 0">暂无服务，点击 <el-button size="mini"
+                                                               icon="el-icon-plus"
+                                                               @click="createService()"
+                                                               plain
+                                                               circle>
+            </el-button> 创建服务</p>
+          </div>
+          <div class = "aside__inner">
+            <serviceAside></serviceAside>
+          </div>
+        </multipane>
+      </div>
+    </div>
 
     <div class="controls__wrap">
       <div class="controls__right">
@@ -52,60 +83,117 @@
   </div>
 </template>
 <script>
-import step from '../common/step.vue'
-import ServiceK8s from './service_k8s'
+  import step from '../common/step.vue'
+  import serviceTree from '../common/service_tree'
+  import serviceAside from './service_aside'
+  import { Multipane, MultipaneResizer } from 'vue-multipane'
+  import axios from 'axios'
 
-import axios from 'axios'
+  export default {
+    components: {
+      step, serviceTree, serviceAside, Multipane, MultipaneResizer
+    },
+    data () {
+      return {
+        service: {},
+        services: [],
+        sharedServices: [],
+        detectedEnvs: [],
+        detectedServices: [],
+        systemEnvs: [],
+        currentServiceYamlKinds: {},
+        projectInfo: {},
+        showNext: false,
+        addCodeDrawer: false,
+        guideItems:[]
+      }
+    },
+    methods: {
+      // createService () {
+      //   this.$refs.serviceTree.createService('platform')
+      // },
+      //
+      // getServices () {
+      //   const projectName = this.projectName
+      //   this.$set(this, 'service', {})
+      //   getServiceTemplatesAPI(projectName).then((res) => {
+      //     this.services = sortBy((res.data.map(service => {
+      //       service.idStr = `${service.service_name}/${service.type}`
+      //       service.status = 'added'
+      //       return service
+      //     })), 'service_name')
+      //   })
+      // },
 
-export default {
-  components: {
-    step,  ServiceK8s
-  },
-  data () {
-    return {
-      service: {},
-      services: [],
-      sharedServices: [],
-      detectedEnvs: [],
-      detectedServices: [],
-      systemEnvs: [],
-      currentServiceYamlKinds: {},
-      projectInfo: {},
-      showNext: false,
-      addCodeDrawer: false,
-      guideItems:[]
+      readGuideItems() {
+        axios.get('/getDescription').then((response) => {
+          if(response.data){
+            this.guideItems = response.data.data
+          }
+        })
+      },
+
+      // getYamlKind (payload) {
+      //   this.currentServiceYamlKinds = payload
+      // },
+      //
+      // jumpToKind (payload) {
+      //   this.$refs.serviceEditor.jumpToWord(`kind: ${payload.kind}`)
+      // }
+    },
+
+    mounted() {
+      // this.getServices()
+      this.readGuideItems()
     }
-  },
-  methods: {
-    readGuideItems() {
-      axios.get('/getDescription').then((response) => {
-        if(response.data){
-          this.guideItems = response.data.data
-        }
-      })
-    }
-
-  },
-
-  mounted() {
-    // this.getServices()
-    this.readGuideItems()
   }
-}
 </script>
 
 <style lang="less">
-  .projects-guide-service-container {
-    height: calc(~'100% - 40px');
+  .fileTree-dialog {
+    .el-dialog__body {
+      padding: 0 5px;
+    }
+  }
 
-    /deep/ .projects-service-mgr {
-      .service-wrap {
-        height: calc(~'100% - 255px');
+  .dialog-upgrade-env {
+    .el-dialog__body {
+      padding: 10px 20px;
+    }
+
+    .title {
+      .el-alert {
+        margin-bottom: 10px;
       }
     }
 
+    .update-policy-container {
+      margin-top: 15px;
+
+      span {
+        color: #4c4c4c;
+        font-size: 14px;
+      }
+    }
+  }
+
+  .projects-service-mgr {
+    position: relative;
+    flex: 1;
+    overflow: auto;
+    background-color: #f5f7f7;
+
+    .project-name {
+      color: #1989fa;
+    }
+
     .guide-container {
+      min-height: calc(~"100% - 150px");
       margin-top: 10px;
+
+      &.not-closed-title {
+        min-height: calc(~"100% - 150px");
+      }
 
       .current-step-container {
         .title-container {
@@ -113,20 +201,121 @@ export default {
 
           .first {
             display: inline-block;
-            width: 130px;
+            width: 110px;
             padding: 8px;
             color: #fff;
             font-weight: 300;
-            font-size: 16px;
+            font-size: 18px;
             text-align: center;
-            background: #0066ff;
+            background: #3289e4;
           }
 
           .second {
+            display: inline-block;
             color: #4c4c4c;
             font-size: 13px;
           }
         }
+      }
+    }
+
+    .pipeline {
+      position: relative;
+      display: flex;
+      height: calc(~'100% - 70px');
+      padding: 15px 5px 0 5px;
+
+      &.onboarding {
+        height: calc(~'100% - 245px');
+      }
+    }
+
+    .pipeline-workflow__wrap {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      border-bottom: 1px solid #ccc;
+
+      .service-tree-container {
+        display: flex;
+        flex-direction: column;
+        min-width: 250px;
+        max-width: 480px;
+        height: 100%;
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.05);
+      }
+
+      .vertical-panes {
+        width: 100%;
+        height: 550px;
+
+        .multipane-resizer {
+          position: relative;
+          left: 0;
+          width: 10px;
+          margin: 0;
+
+          &::before {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            display: block;
+            width: 8px;
+            height: 55px;
+            margin-top: -20px;
+            margin-left: -5.5px;
+            background-color: #fff;
+            border: 1px solid #dbdbdb;
+            border-radius: 5px;
+            content: '';
+          }
+
+          &:hover {
+            &::before {
+              border-color: #999;
+            }
+          }
+        }
+      }
+
+      .service-editor-container {
+        height: 100%;
+
+        &.pm {
+          width: 100%;
+        }
+      }
+
+      .no-content {
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+
+        img {
+          width: 100px;
+          height: 100px;
+        }
+
+        p {
+          color: #606266;
+          font-size: 15px;
+        }
+      }
+
+      .pipelines__aside_right {
+        min-width: 372px;
+        -webkit-transition: width 0.2s ease-out;
+        transition: width 0.2s ease-out;
+      }
+
+      .pipelines__aside {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
       }
     }
 
@@ -139,20 +328,19 @@ export default {
       display: flex;
       align-items: center;
       justify-content: space-between;
-      height: 60px;
-      margin: 0 15px;
-      padding: 0 10px;
+      height: 55px;
+      margin: 0 5px;
       background-color: #fff;
       box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.05);
 
+      > * {
+        margin-right: 10px;
+      }
+
       .controls__right {
-        display: -webkit-box;
-        display: -ms-flexbox;
         display: flex;
         align-items: center;
-        margin-right: 10px;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
+        padding: 0 15px;
 
         .save-btn {
           margin-right: 15px;
@@ -176,3 +364,4 @@ export default {
     }
   }
 </style>
+
