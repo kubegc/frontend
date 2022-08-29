5b1467158a7fa5ab0a533ce63ca51ec23705fedd
yanbingshuai <1340660560@qq.com>
2022-08-29 18:20:21 +0800

    aaa

--- src/views/pipeline/detail_ope/view.vue
+++ src/views/pipeline/detail_ope/view.vue
@@ -2,6 +2,47 @@
   <div class="projects-container">
     <SubTopbar :comp="comp" :projectName="projectName" />
     <router-view @injectComp="injectComp" ></router-view>
+
+    <div class="init-resource">
+      <header><i class="theme-color iconfont iconvery-handshake"></i>管理员为您配置了以下工作流和环境</header>
+      <section>
+        <article>
+          <div class="title">
+            <i class="iconfont icongongzuoliucheng"></i>工作流
+          </div>
+          <div class="detail-item">
+            <div class="item-name">{{ workflow.name }}</div>
+          </div>
+        </article>
+        <article  style="margin-top: 30px;">
+          <div class="title">
+            <i class="iconfont iconvery-environ"></i>环境
+          </div>
+          <div  class="detail-item display-flex">
+            <div>
+              <div class="item-name">{{ env.name }}</div>
+            </div>
+            <el-button type="primary" plain size="small" >环境变量</el-button>
+          </div>
+        </article>
+
+        <footer>
+          <el-button type="primary" size="small" icon="confirm-icon iconfont iconvery-confirm"  round>确认</el-button>
+        </footer>
+      </section>
+    </div>
+
+      <template>
+        <el-dialog :title="`设置环境变量`" :visible.sync="dialogVisible" width="850px">
+          <div>
+            <VarList :variables="variables" class="var-list-container"></VarList>
+          </div>
+          <div slot="footer">
+            <el-button size="small" type="primary" @click="getEnvInfo">确 定</el-button>
+          </div>
+        </el-dialog>
+      </template>
+
   </div>
 </template>
 <script>
@@ -9,7 +50,8 @@ import SubTopbar from '../home/subTopbar'
 export default {
   data () {
     return {
-      comp: null
+      comp: null,
+      currentInfo: Object
     }
   },
   components: {
@@ -18,11 +60,29 @@ export default {
   computed: {
     projectName () {
       return this.$route.params.project_name
+    },
+    dialogVisible: {
+      get() {
+        return this.visible
+      },
     }
   },
   methods: {
     injectComp (comp) {
       this.comp = comp
+    },
+    getEnvInfo () {
+      if (this.deployType === 'k8s') {
+        this.currentInfo.vars = cloneDeep(this.variables)
+      } else {
+        const {
+          envInfo,
+          chartInfo
+        } = this.$refs.helmEnvTemplateRef.getAllInfo()
+        this.currentInfo.defaultValues = envInfo.DEFAULT || ''
+        this.currentInfo.chartValues = chartInfo
+      }
+      this.dialogVisible = false
     }
   }
 }
